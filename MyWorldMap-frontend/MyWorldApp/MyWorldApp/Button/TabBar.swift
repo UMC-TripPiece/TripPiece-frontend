716cc56724809d76b9372e03f794bf74dc836a8a
Yeonnies <eliyeon16@gmail.com>
2024-08-14 14:39:02 +0900

    Fix: Tab Bar

--- MyWorldMap-frontend/MyWorldApp/MyWorldApp/Button/TabBar.swift
+++ MyWorldMap-frontend/MyWorldApp/MyWorldApp/Button/TabBar.swift
@@ -19,6 +19,7 @@ func setTabBar() {
         //TODO - 여기서 월드 부분 뭐시기 컨트롤러? 뭐 그런걸로 해야 스유랑 연결됨
         let vc1 = UINavigationController(rootViewController: WorldViewController())
         vc1.tabBarItem = UITabBarItem(title: "월드", image: UIImage(named: "World"), tag: 1)
+        //let vc2 = UINavigationController(rootViewController: MyLogViewController())
         let vc2 = UINavigationController(rootViewController: LogViewController())
         vc2.tabBarItem = UITabBarItem(title: "나의 기록", image: UIImage(named: "My log"), tag: 2)
         let vc3 = UINavigationController(rootViewController: SearchViewController())
@@ -29,8 +30,33 @@ func setTabBar() {
     }
     private func appearance() {
         let barAppearance = UITabBarAppearance()
-        barAppearance.stackedLayoutAppearance.selected.iconColor = .systemBlue
+        barAppearance.configureWithOpaqueBackground()
+
+        // 아이콘 기본 색상 설정
+        barAppearance.stackedLayoutAppearance.normal.iconColor = UIColor(named: "Not selected")
+
         self.tabBar.standardAppearance = barAppearance
+        self.tabBar.layer.cornerRadius = 20
+        self.tabBar.layer.masksToBounds = false
+        self.tabBar.tintColor = UIColor(named: "Main")
         self.tabBar.backgroundColor = .white
+
+        // 그림자 설정
+        self.tabBar.layer.shadowColor = UIColor.black.cgColor
+        self.tabBar.layer.shadowOpacity = 0.2
+        self.tabBar.layer.shadowOffset = CGSize(width: 0, height: -2)
+        self.tabBar.layer.shadowRadius = 10
+
+        // 탭바 아이템 위치 조정
+        self.tabBar.itemPositioning = .centered
+        self.tabBar.itemSpacing = 55
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        var tabFrame = tabBar.frame
+        tabFrame.size.height = 100
+        tabFrame.origin.y = view.frame.size.height - 100
+        tabBar.frame = tabFrame
     }
 }
--- MyWorldMap-frontend/MyWorldApp/MyWorldApp/My Log/Start Log/StartLogViewController.swift
+++ MyWorldMap-frontend/MyWorldApp/MyWorldApp/My Log/Start Log/StartLogViewController.swift
@@ -6,10 +6,10 @@
 //
 
 import UIKit
-import SnapKit
 import Alamofire
 
 class StartLogViewController: UIViewController {
+    
     //MARK: - UI
     private lazy var startNavBar: LogStartNavigationBar = {
         let nav = LogStartNavigationBar()
@@ -155,6 +155,17 @@ class StartLogViewController: UIViewController {
         return searchController
     }()
     
+    ///검색결과
+    private var searchResults: [[String: String]] = []
+    private lazy var searchTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.isHidden = true
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+
     //MARK: - Init
     override func viewDidLoad() {
         self.view.addSubview(startNavBar)
@@ -173,6 +184,7 @@ class StartLogViewController: UIViewController {
         view.addSubview(addCountryButton)
         view.addSubview(titleLabel)
         view.addSubview(grayBackgroundView)
+        view.addSubview(searchTableView)
         grayBackgroundView.addSubview(contentStackView)
         
         contentStackView.addArrangedSubview(createSpacer(height: 30))
@@ -227,11 +239,16 @@ class StartLogViewController: UIViewController {
         }
         
         travelTextView.snp.makeConstraints { make in
-            make.height.equalTo(50)
+            make.height.equalTo(50) // 원하는 높이 설정
         }
         
         startLogButton.snp.makeConstraints { make in
             make.height.equalTo(44)
+        }
+        searchTableView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(21)
+            make.top.equalTo(grayBackgroundView.snp.top).offset(20)
+            make.bottom.equalToSuperview().offset(-20)
         }
     }
     
@@ -288,6 +305,36 @@ class StartLogViewController: UIViewController {
     @objc private func dismissKeyboard() {
         view.endEditing(true)
     }
+    
+    //MARK: - Alamofire
+    private func searchCities(keyword: String) {
+        let url = "http://3.34.123.244:8080/search/cities"
+        let parameters: [String: String] = ["keyword": keyword]
+            
+        AF.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default).responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                if let data = response.data, let jsonString = String(data: data, encoding: .utf8) {
+                    print("전체 JSON 응답: \(jsonString)")
+                    if let json = value as? [String: Any], let cities = json["result"] as? [[String: Any]] {
+                        self.searchResults = cities.map { cityData in
+                            var cityDataDecoded = [String: String]()
+                            cityData.forEach { key, value in
+                                if let stringValue = value as? String {
+                                    cityDataDecoded[key] = stringValue
+                                }
+                            }
+                            return cityDataDecoded
+                        }
+                        self.searchTableView.isHidden = false
+                        self.searchTableView.reloadData()
+                    }
+                }
+            case .failure(let error):
+                print("Error: \(error)")
+            }
+        }
+    }
 }
 
 //MARK: - UITextViewDelegate
@@ -326,6 +373,13 @@ extension StartLogViewController: UISearchResultsUpdating {
             return
         }
         searchCities(keyword: keyword)
+    }
+}
+
+//MARK: - UITableViewDataSource, UITableViewDelegate
+extension StartLogViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResults.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {

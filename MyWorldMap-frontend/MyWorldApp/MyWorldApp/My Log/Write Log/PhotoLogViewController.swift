4f459131c55e15b8d511e9d64ab979305a20b81e
Yeonnies <eliyeon16@gmail.com>
2024-08-22 11:32:24 +0900

    Add: PhotoLogVC

--- MyWorldMap-frontend/MyWorldApp/MyWorldApp/My Log/Write Log/PhotoLogViewController.swift
+++ MyWorldMap-frontend/MyWorldApp/MyWorldApp/My Log/Write Log/PhotoLogViewController.swift
@@ -7,20 +7,21 @@
 
 import UIKit
 import SnapKit
+import Alamofire
 
 class PhotoLogViewController: UIViewController {
-    
+
     var travelId: Int
-        
+
     init(travelId: Int) {
         self.travelId = travelId
         super.init(nibName: nil, bundle: nil)
     }
-        
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     //MARK: - UI
     private lazy var customNavBar: CustomNavigationBar = {
         let nav = CustomNavigationBar()
@@ -28,14 +29,14 @@ class PhotoLogViewController: UIViewController {
         nav.backgroundColor = .white
         return nav
     }()
-    
+
     private lazy var titleLabel: UILabel = {
         let label = UILabel()
         label.text = "사진 추가"
         label.font = UIFont.systemFont(ofSize: 28, weight: .bold)
         return label
     }()
-    
+
     private lazy var subtitleLabel: UILabel = {
         let label = UILabel()
         label.text = "사진을 추가한 후 \n설명을 작성해보세요!"
@@ -45,27 +46,26 @@ class PhotoLogViewController: UIViewController {
         label.textColor = UIColor(named: "subtitle")
         return label
     }()
-    
+
     private lazy var titleImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "dum1")
         return imageView
     }()
-        
-    ///회색 배경부분부터
+
     private lazy var grayBackgroundView: UIView = {
         let view = UIView()
         view.backgroundColor = UIColor(named: "BgColor2")
         return view
     }()
-    
+
     private lazy var contentStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .vertical
         stackView.alignment = .fill
         return stackView
     }()
-    
+
     private lazy var photosStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .horizontal
@@ -73,7 +73,7 @@ class PhotoLogViewController: UIViewController {
         stackView.distribution = .fillEqually
         return stackView
     }()
-    
+
     private lazy var photoSubtitleLabel: UILabel = {
         let label = UILabel()
         label.text = "최대 4장 첨부 가능합니다"
@@ -82,7 +82,7 @@ class PhotoLogViewController: UIViewController {
         label.textColor = UIColor(named: "Black2")
         return label
     }()
-    
+
     private lazy var memoTitleLabel: UILabel = {
         let label = UILabel()
         label.text = "메모"
@@ -90,7 +90,7 @@ class PhotoLogViewController: UIViewController {
         label.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
         return label
     }()
-    
+
     private lazy var memoTextView: UITextView = {
         let textView = UITextView()
         textView.font = UIFont.systemFont(ofSize: 16)
@@ -104,7 +104,7 @@ class PhotoLogViewController: UIViewController {
         textView.delegate = self
         return textView
     }()
-        
+
     private lazy var addButton: UIButton = {
         let button = UIButton()
         button.setTitle("기록 추가", for: .normal)
@@ -114,7 +114,7 @@ class PhotoLogViewController: UIViewController {
         button.addTarget(self, action: #selector(addRecord), for: .touchUpInside)
         return button
     }()
-        
+
     private var selectedImages: [UIImage] = [] {
         didSet {
             updateAddButtonState()
@@ -129,7 +129,6 @@ class PhotoLogViewController: UIViewController {
         self.view.addSubview(customNavBar)
         setupUI()
 
-        // NotificationCenter 관찰자 추가
         NotificationCenter.default.addObserver(self, selector: #selector(handleBackButtonTap), name: .backButtonTapped, object: nil)
     }
 
@@ -140,7 +139,7 @@ class PhotoLogViewController: UIViewController {
         view.addSubview(titleImageView)
         view.addSubview(photosStackView)
         view.addSubview(addButton)
-        
+
         view.addSubview(grayBackgroundView)
         grayBackgroundView.addSubview(contentStackView)
         contentStackView.addArrangedSubview(photosStackView)
@@ -156,14 +155,14 @@ class PhotoLogViewController: UIViewController {
         setConstraints()
         setupPhotosStackView()
     }
-    
+
     private func createSpacer(height: CGFloat) -> UIView {
         let spacer = UIView()
         spacer.translatesAutoresizingMaskIntoConstraints = false
         spacer.heightAnchor.constraint(equalToConstant: height).isActive = true
         return spacer
     }
-    
+
     func setConstraints() {
         customNavBar.snp.makeConstraints { make in
             make.leading.trailing.equalToSuperview()
@@ -188,21 +187,21 @@ class PhotoLogViewController: UIViewController {
             make.leading.trailing.equalToSuperview()
             make.bottom.equalToSuperview()
         }
-                
+
         contentStackView.snp.makeConstraints { make in
             make.top.equalTo(grayBackgroundView.snp.top).offset(30)
             make.leading.trailing.equalToSuperview().inset(21)
         }
-        
+
         memoTextView.snp.makeConstraints { make in
             make.height.equalTo(154)
         }
-        
+
         addButton.snp.makeConstraints { make in
             make.height.equalTo(50) // 원하는 버튼 높이
         }
     }
-    
+
     //MARK: - Function
     private func setupPhotosStackView() {
         for _ in 0..<4 {
@@ -210,30 +209,38 @@ class PhotoLogViewController: UIViewController {
             imageView.image = UIImage(named: "add photo")
             imageView.tintColor = .gray
             imageView.contentMode = .scaleAspectFit
+            imageView.layer.cornerRadius = 10
+            imageView.clipsToBounds = true
             imageView.layer.shadowColor = UIColor.black.cgColor
             imageView.layer.shadowOpacity = 0.2
             imageView.layer.shadowOffset = CGSize(width: 0, height: 2)
             imageView.isUserInteractionEnabled = true
+
+            imageView.snp.makeConstraints { make in
+                make.width.height.equalTo(80) // 크기를 80x80으로 설정
+            }
+
             let tapGesture = UITapGestureRecognizer(target: self, action: #selector(selectPhoto))
             imageView.addGestureRecognizer(tapGesture)
             photosStackView.addArrangedSubview(imageView)
         }
     }
-    
+
     private func updateAddButtonState() {
-        addButton.isEnabled = !selectedImages.isEmpty
-        addButton.backgroundColor = selectedImages.isEmpty ? UIColor(named: "Cancel") : UIColor(named: "Main")
+        let isMemoValid = !memoTextView.text.isEmpty && memoTextView.text != "| 사진에 대해 설명해주세요!"
+        addButton.isEnabled = !selectedImages.isEmpty && isMemoValid
+        addButton.backgroundColor = (selectedImages.isEmpty || !isMemoValid) ? UIColor(named: "Cancel") : UIColor(named: "Main")
     }
-    
+
     ///뒤로가기 버튼
     @objc private func handleBackButtonTap() {
         self.dismiss(animated: true, completion: nil)
     }
-    
+
     ///사진 추가 버튼 클릭
     @objc private func selectPhoto() {
         let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-            
+
         let cameraAction = UIAlertAction(title: "카메라 열기", style: .default) { _ in
             if UIImagePickerController.isSourceTypeAvailable(.camera) {
                 let imagePickerController = UIImagePickerController()
@@ -241,10 +248,9 @@ class PhotoLogViewController: UIViewController {
                 imagePickerController.sourceType = .camera
                 self.present(imagePickerController, animated: true, completion: nil)
             } else {
-                // 카메라 사용 불가 처리
                 let alert = UIAlertController(title: "카메라 사용 불가", message: "카메라를 사용할 수 없습니다.", preferredStyle: .alert)
                 let okAction = UIAlertAction(title: "확인", style: .default, handler: nil)
-                    alert.addAction(okAction)
+                alert.addAction(okAction)
                 self.present(alert, animated: true, completion: nil)
             }
         }
@@ -255,42 +261,106 @@ class PhotoLogViewController: UIViewController {
             self.present(imagePickerController, animated: true, completion: nil)
         }
         let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
-            alert.addAction(cameraAction)
-            alert.addAction(libraryAction)
-            alert.addAction(cancelAction)
-            present(alert, animated: true, completion: nil)
-    }
-    
-    @objc private func addRecord() {
-        // 기록 추가 기능 구현
-    }
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        if let selectedImage = info[.originalImage] as? UIImage {
-            if selectedImages.count < 4 {
-                selectedImages.append(selectedImage)
-                let imageView = photosStackView.arrangedSubviews[selectedImages.count - 1] as! UIImageView
-                imageView.image = selectedImage
-            }
-        }
-        picker.dismiss(animated: true, completion: nil)
-    }
-    
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        picker.dismiss(animated: true, completion: nil)
+        alert.addAction(cameraAction)
+        alert.addAction(libraryAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true, completion: nil)
     }
 
-    deinit {
-        NotificationCenter.default.removeObserver(self, name: .backButtonTapped, object: nil)
+    @objc private func addRecord() {
+        postPhotoLog()
+    }
+
+    //MARK: - POST 요청
+    private func postPhotoLog() {
+        guard let url = URL(string: "http://3.34.123.244:8080/mytravels/\(travelId)/picture") else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("multipart/form-data", forHTTPHeaderField: "Content-Type")
+        
+        if let refreshToken = getRefreshToken() {
+            request.setValue("Bearer \(refreshToken)", forHTTPHeaderField: "Authorization")
+        }
+        
+        let boundary = UUID().uuidString
+        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+        
+        let body = NSMutableData()
+        
+        // memo 부분
+        let memoDescription = [
+            "description": memoTextView.text ?? ""
+        ]
+        
+        if let memoData = try? JSONSerialization.data(withJSONObject: memoDescription, options: []) {
+            body.append("--\(boundary)\r\n".data(using: .utf8)!)
+            body.append("Content-Disposition: form-data; name=\"memo\"\r\n".data(using: .utf8)!)
+            body.append("Content-Type: application/json\r\n\r\n".data(using: .utf8)!)
+            body.append(memoData)
+            body.append("\r\n".data(using: .utf8)!)
+        }
+        
+        // photos 부분, 압축률을 0.5로 낮춤
+        for (index, image) in selectedImages.enumerated() {
+            if let imageData = image.jpegData(compressionQuality: 0.5) {
+                body.append("--\(boundary)\r\n".data(using: .utf8)!)
+                body.append("Content-Disposition: form-data; name=\"photos\"; filename=\"photo\(index).jpg\"\r\n".data(using: .utf8)!)
+                body.append("Content-Type: image/jpeg\r\n\r\n".data(using: .utf8)!)
+                body.append(imageData)
+                body.append("\r\n".data(using: .utf8)!)
+            }
+        }
+        
+        body.append("--\(boundary)--\r\n".data(using: .utf8)!)
+        request.httpBody = body as Data
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+                return
+            }
+            
+            if let httpResponse = response as? HTTPURLResponse {
+                print("HTTP Status Code: \(httpResponse.statusCode)")
+            }
+            
+            guard let data = data else {
+                print("No data received.")
+                return
+            }
+            
+            if let responseString = String(data: data, encoding: .utf8) {
+                print("Response: \(responseString)")
+            } else {
+                print("Failed to decode response.")
+            }
+            
+            DispatchQueue.main.async {
+                self.navigateToPhotoCompleteViewController()
+            }
+        }
+        task.resume()
+    }
+
+    private func getRefreshToken() -> String? {
+        return UserDefaults.standard.string(forKey: "refreshToken")
     }
     
-    ///키보드 내리기
+    var videoImage: UIImage?
+    private func navigateToPhotoCompleteViewController() {
+        let recordCompleteVC = PhotoCompleteViewController()
+        recordCompleteVC.images = selectedImages
+        recordCompleteVC.memoText = memoTextView.text
+        recordCompleteVC.modalPresentationStyle = .fullScreen
+        present(recordCompleteVC, animated: true, completion: nil)
+    }
+    
     private func setupDismissKeyboardGesture() {
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         tapGesture.cancelsTouchesInView = false
         view.addGestureRecognizer(tapGesture)
     }
-        
     @objc private func dismissKeyboard() {
         view.endEditing(true)
     }
@@ -310,5 +380,33 @@ extension PhotoLogViewController: UITextViewDelegate, UIImagePickerControllerDel
             textView.text = "| 사진에 대해 설명해주세요!"
             textView.textColor = UIColor(named: "Black3")
         }
+    }
+
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        let currentText = textView.text ?? ""
+        guard let stringRange = Range(range, in: currentText) else { return false }
+        let updatedText = currentText.replacingCharacters(in: stringRange, with: text)
+        return updatedText.count <= 30 // 글자 수 제한 30자
+    }
+
+    func textViewDidChange(_ textView: UITextView) {
+        updateAddButtonState()
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[.originalImage] as? UIImage {
+            if selectedImages.count < 4 {
+                selectedImages.append(selectedImage)
+                let imageView = photosStackView.arrangedSubviews[selectedImages.count - 1] as! UIImageView
+                imageView.image = selectedImage
+                imageView.contentMode = .scaleAspectFill
+                imageView.clipsToBounds = true
+            }
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
     }
 }
